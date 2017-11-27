b8b6a499bc76ed343b27c3d96eb6a6d9931f4600
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2017-11-27 12:56:39 -0800

    started celery int

---
+++ tasks.py
@@ -0,0 +1,9 @@
+import celery
+import os
+app = celery.Celery('example')
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+                
+@app.task
+def add(x, y):
+    return x + y
