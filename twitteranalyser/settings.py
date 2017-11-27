8dc2e6904fccebb93aa55b45443a3af31bf9dd00
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2017-11-27 11:16:47 -0800

    fix config

--- twitteranalyser/settings.py
+++ twitteranalyser/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'twitteranalyser.wsgi.application'
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES = { 'default': db_from_env }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
