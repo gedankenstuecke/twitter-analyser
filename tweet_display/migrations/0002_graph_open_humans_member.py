63f820320fd1fb94e2982f1bf0dc793a7301c469
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2017-12-13 14:57:42 -0800

    migration

--- tweet_display/migrations/0002_graph_open_humans_member.py
+++ tweet_display/migrations/0002_graph_open_humans_member.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='graph',
             name='open_humans_member',
-            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, to='users.OpenHumansMember'),
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='users.OpenHumansMember'),
             preserve_default=False,
         ),
     ]
