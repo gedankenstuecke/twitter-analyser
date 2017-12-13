43731941942ccb1b8f53a8fb77674d16c09bfcab
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2017-12-13 14:55:53 -0800

    migration

--- tweet_display/models.py
+++ tweet_display/models.py
@@ -6,7 +6,9 @@ class Graph(models.Model):
     graph_type = models.CharField(max_length=200)
     graph_description = models.CharField(max_length=200)
     graph_data = models.TextField()
-    open_humans_member = models.ForeignKey(OpenHumansMember, on_delete=models.CASCADE)
+    open_humans_member = models.ForeignKey(OpenHumansMember,
+                                           blank=True, null=True,
+                                           on_delete=models.CASCADE)
 
     def __str__(self):
         return self.graph_type + ': ' + self.graph_description
