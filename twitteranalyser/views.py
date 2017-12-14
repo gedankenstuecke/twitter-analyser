07fdfeca8282cadeaee6587a3372d8097a2f7945
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2017-12-14 15:35:23 -0800

    added to faq

--- twitteranalyser/views.py
+++ twitteranalyser/views.py
@@ -1,7 +1,9 @@
 # from django.http import HttpResponse
 from django.shortcuts import render  # redirect
+from django.conf import settings
 
 
 def about(request):
-    context = {'section': 'about'}
+    context = {'section': 'about',
+               'oh_proj_page': settings.OH_ACTIVITY_PAGE}
     return render(request, 'twitteranalyser/about.html', context)
